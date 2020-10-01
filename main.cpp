53d95208202716d0567057f30569ab4402988345
rytisvaidziulis <71770033+rytisvaidziulis@users.noreply.github.com>
2020-10-01 13:36:49 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//PIRMOJi UZDUOTIS
+#include <iostream>
+#include <clocale>
+
+using namespace std;
+int main() {
+    cout << "2020" << endl;
+    cout << "Kiek Jums metu?" << endl;
+
+    return 0;
+}
